503e65e5483e66f76ac299495dd992cf959bcd92
sirmcclay <mcclay.design@gmail.com>
2020-11-24 12:14:36 -0300

    trigger() method to trigger events and so

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'my name', age: 20 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('slfksjflk', () => {});
+user.on('change', () => {
+	console.log('Change #1');
+});
+user.on('change', () => {
+	console.log('Change #2');
+});
+user.on('save', () => {
+	console.log('Save was triggered');
+});
 
-console.log(user);
+user.trigger('change');
+user.trigger('save');
