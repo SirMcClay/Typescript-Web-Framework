47def37c0363b4ce7c1a956e7e636ad49f9c9ee9
sirmcclay <mcclay.design@gmail.com>
2020-11-24 13:03:54 -0300

    Test json-server using axios

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'my name', age: 20 });
-
-user.on('change', () => {
-	console.log('Change #1');
-});
-user.on('change', () => {
-	console.log('Change #2');
-});
-user.on('save', () => {
-	console.log('Save was triggered');
+axios.post('http://localhost:3000/users', {
+	name: 'my name',
+	age: '20',
 });
 
-user.trigger('change');
-user.trigger('save');
+axios.get('http://localhost:3000/users/1');
