4f17ebc3ca79fad9ed726563c282285c2e17e364
sirmcclay <mcclay.design@gmail.com>
2020-11-26 12:36:52 -0300

    Refactor: Reimplement fetch() method on User class and test on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Another User', age: 888 });
-
-console.log(user.get('name'));
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-	console.log('User was changed');
+	console.log(user);
 });
 
-user.set({ name: 'new name ok' });
+user.fetch();
