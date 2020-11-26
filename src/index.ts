c9c532a9de4fd5335a0f546b949d70a85ccba323
sirmcclay <mcclay.design@gmail.com>
2020-11-26 12:17:37 -0300

    Refactor: Change methods that use this on classes Attributes and Eventing to use arrow functions because of a this context issue

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Another User', age: 888 });
 
+console.log(user.get('name'));
+
 user.on('change', () => {
-	console.log('user was change');
+	console.log('User was changed');
 });
+
+user.trigger('change');
